9e4404f98b8473cb0050996b1c9d19c12297e20d
范义辉 <fanyihui@tuhu.cn>
2021-10-11 09:43:52 +0800

    changes

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,4 +1,5 @@
 
 module.exports = function(argv) {
-  console.log('%c 🍗 argv: ', 'font-size:20px;background-color: #B03734;color:#fff;', argv);
+  console.log('%c 🌶 argv: ', 'font-size:20px;background-color: #33A5FF;color:#fff;', argv);
+  console.log('%c 🍇 argv: ', 'font-size:20px;background-color: #93C0A4;color:#fff;', argv);
 }
